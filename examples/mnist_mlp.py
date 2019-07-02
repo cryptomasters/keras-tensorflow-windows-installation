083930c852532b14374a4e14e482c77c7fd2bf87
cryptomasters <30029756+cryptomasters@users.noreply.github.com>
2019-07-02 10:50:50 +0700

    1
    1

--- examples/mnist_mlp.py
+++ examples/mnist_mlp.py
@@ -29,7 +29,7 @@ x_test /= 255
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
-# convert class vectors to binary class matrices
+# convert class vectors to binary class matrices 
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
